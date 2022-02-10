445dea9612f5b534559675058db5c4a40879fb86
sykang071 <kang.seu@northeastern.edu>
2022-02-10 13:36:40 -0500

    ic 06

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ Prof. Mosca
 Modified: 12/06/21 
 
 */
+function returnRand() {
+    let myRand = Math.random()*100;
+    return myRand;
+}
 
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
+    let generateNum = returnRand();
+    let buttonDiv  
     console.log("Button 1 was clicked"); 
 }
